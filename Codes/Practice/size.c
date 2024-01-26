8d45705bcd75c27c18041dcf058d7e625fe76ec2
sheteshreyash <shreyashshete21@gmail.com>
2024-01-26 19:48:06 +0530

    new changes

--- Codes/Practice/size.c
+++ Codes/Practice/size.c
@@ -1,7 +1,5 @@
     // c program to find the size of the variable
-
     #include <stdio.h>
-    #include <stdlib.h>
 
     int main(int argc, char const *argv[])
     {
@@ -10,7 +8,7 @@
         do {
             printf("\n----C program to find the size of the variable---");
             printf("\n1 - Integer\n2 - Double\n3 - Float\n4 - Character");
-            printf("\nEnter your choice : ");
+            printf("\nEnter your choice : (-1 to exit program)");
             scanf("%d", &ch);
 
             switch (ch)
@@ -19,32 +17,26 @@
             printf("Size of integer is : \n");
             printf("%lu", sizeof(int));
             break;
-
             case 2 :
             printf("\nSize of Double is : \n");
             printf("%lu", sizeof(double));
             break;
-
             case 3 :
             printf("\nSize of Float is : \n");
             printf("%lu", sizeof(float));
             break;
-
             case 4 :
             printf("\nSize of Character is : \n");
             printf("%lu", sizeof(char));
             break;
-            
             default:
             printf("\nInvalid choice !!");
             break;
             }
-
-            printf("\nDo you want to continue ? press 1");
+            printf("\nDo you want to continue ? press 1 \n");
             scanf("%d", &ch);
-        
+
         }while(ch == 1);
-        printf("Thank you...");    
-    
+        printf("Thank you...");
         return 0;
     }
