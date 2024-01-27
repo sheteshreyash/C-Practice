7a633f24e7a460bc9c416025c4a20b8dc9c6d37d
sheteshreyash <shreyashshete21@gmail.com>
2024-01-27 18:31:21 +0530

    new changes chapter 2

---
+++ Codes/chapter 2/Instructions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a = 4, b, c; // declarations
+    b = c = a;
+    printf("value of a is %d\n", a);
+    printf("value of b is %d\n", b);
+    printf("value of c is %d\n", c);
+    return 0;
+}
